a292baea867d2e8a74779155b61ae8abc3ba3cc3
Joel Akeret <jakeret@phys.ethz.ch>
2016-11-01 18:00:38 +0100

    using GPLv3

--- scripts/ufig_util.py
+++ scripts/ufig_util.py
@@ -1,4 +1,16 @@
-# Copyright (C) 2015 ETH Zurich, Institute for Astronomy
+# tf_unet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# tf_unet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with tf_unet.  If not, see <http://www.gnu.org/licenses/>.
+
 
 '''
 Created on Aug 30, 2016
