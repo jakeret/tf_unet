1f2f63760137e7383cc822ff644d7d33d18b9cc2
Joel Akeret <jakeret@phys.ethz.ch>
2016-12-13 22:01:24 +0100

    using int division to avoid exception

--- tf_unet/layers.py
+++ tf_unet/layers.py
@@ -48,7 +48,7 @@ def crop_and_concat(x1,x2):
     x1_shape = tf.shape(x1)
     x2_shape = tf.shape(x2)
     # offsets for the top left corner of the crop
-    offsets = [0, (x1_shape[1] - x2_shape[1]) / 2, (x1_shape[2] - x2_shape[2]) / 2, 0]
+    offsets = [0, (x1_shape[1] - x2_shape[1]) // 2, (x1_shape[2] - x2_shape[2]) // 2, 0]
     size = [-1, x2_shape[1], x2_shape[2], -1]
     x1_crop = tf.slice(x1, offsets, size)
     return tf.concat(3, [x1_crop, x2])
