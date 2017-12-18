a64c9a919deec0e8e4379e8f0941999a0bf4bfa1
Joel Akeret <joel.akeret@gmail.com>
2017-12-18 19:12:23 +0100

    tidy up

--- batch/unet.sh
+++
@@ -1,4 +0,0 @@
-source $SCRATCH/virtualenvs/env-tensorflow-0.9.0/bin/activate
-export LD_LIBRARY_PATH=/apps/daint/UES/5.2.UP04/sandbox-ds/tensorflow/cudadnn/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/apps/daint/UES/5.2.UP04/sandbox-ds/tensorflow/cudadnn/lib64/:$LD_LIBRARY_PATH
-srun time python ../scripts/rfi_launcher.py
