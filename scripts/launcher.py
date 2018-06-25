45f379e3d31e663ff5c8073b91344700f7ab6026
Joel Akeret <joel.akeret@gmail.com>
2018-06-25 18:47:29 +0200

    simplified demo

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -18,39 +18,31 @@ Created on Jul 28, 2016
 author: jakeret
 '''
 from __future__ import print_function, division, absolute_import, unicode_literals
+import numpy as np
 from tf_unet import image_gen
 from tf_unet import unet
 from tf_unet import util
 
 
 if __name__ == '__main__':
-    nx = 572
-    ny = 572
-     
-    training_iters = 20
-    epochs = 100
-    dropout = 0.75 # Dropout, probability to keep units
-    display_step = 2
-    restore = False
- 
-    generator = image_gen.RgbDataProvider(nx, ny, cnt=20, rectangles=False)
+    np.random.seed(9876)
+
+    generator = image_gen.GrayScaleDataProvider(nx=572, ny=572, cnt=20, rectangles=False)
     
     net = unet.Unet(channels=generator.channels, 
                     n_class=generator.n_class, 
-                    layers=3, 
-                    features_root=16,
-                    cost="dice_coefficient")
+                    layers=3,
+                    features_root=16)
     
     trainer = unet.Trainer(net, optimizer="momentum", opt_kwargs=dict(momentum=0.2))
-    path = trainer.train(generator, "./unet_trained", 
-                         training_iters=training_iters, 
-                         epochs=epochs, 
-                         dropout=dropout, 
-                         display_step=display_step, 
-                         restore=restore)
+    path = trainer.train(generator, "./unet_trained",
+                         training_iters=32,
+                         epochs=5,
+                         dropout=0.75,# probability to keep units
+                         display_step=2)
      
     x_test, y_test = generator(4)
     prediction = net.predict(path, x_test)
      
-    print("Testing error rate: {:.2f}%".format(unet.error_rate(prediction, util.crop_to_shape(y_test, prediction.shape))))
-    
+    print("Testing error rate: {:.2f}%".format(unet.error_rate(prediction,
+                                                               util.crop_to_shape(y_test, prediction.shape))))
